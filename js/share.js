1e2f4c6395ef94e4bde236abdc544ac308d3e89f
dhgkdud12 <dhgkdud14@gmail.com>
2021-08-12 04:09:27 +0900

    결과 카카오톡으로 공유

--- js/share.js
+++ js/share.js
@@ -1,3 +1,44 @@
+const url = 'https://twelvelovetyoe.netlify.app/';
+
+function setShare(){
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '십이간지 연애유형 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-'+resultAlt + '.png';
+  const shareURL = url + 'page.result-' + resultAlt + '.html';
+
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl:
+        shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        WebUrl: shareURL
+      },
+    },
+    social: {
+      likeCount: 10,
+      commentCount: 20,
+      sharedCount: 30,
+    },
+    buttons: [
+      {
+        title: '결과확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+          WebUrl: shareURL
+        },
+      },
+
+    ]
+  });
+
+}
+
 function kakaoShare(){
   Kakao.Link.sendDefault({
     objectType: 'feed',
